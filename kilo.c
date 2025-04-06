864c45b1e2d22023895873fe727daa6716d57845
Adam Kokhnover <adam.kokhnover@gmail.com>
2025-04-06 16:04:46 +0300

    DisableRawMode
    saves the original terminal attributes and restores them when the program exits

--- kilo.c
+++ kilo.c
@@ -1,26 +1,34 @@
 #include <termios.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-/*enables raw mode which turns of echoing when the user types
+
+struct termios orig_termios; //will hold a copy of the original state of the terminal atts
+
+/* resotres the user's original state of the terminal attributes*/
+void disableRawMode() {
+ tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+/*enables raw mode which turns off echoing when the user types
   does this by changing the fourth bit which corresponds to the ECHO bitflag
 */
 void enableRawMode() {
-    struct termios raw; //terminal attributes will be read into this struct
+  tcgetattr(STDIN_FILENO, &orig_termios); //read terminal attributes into the termios struct
+  atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw); //read terminal attributes into the termios struct
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO); //change the fourth bit in the 'local flags' to 0 using NOT, AND.
 
-    raw.c_lflag &= ~(ECHO); //change the fourth bit in the 'local flags' to 0 using NOT, AND.
-
-    //TCSAFLUSH - when to apply the change
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //apply the modified attributes to the terminal
+  //TCSAFLUSH - when to apply the change
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //apply the modified attributes to the terminal
 }
 
 int main() {
-    enableRawMode();
-    char c;
-    printf("LOL");
-    //read 1 byte into var c until there are no more bytes to read or 'q' is pressed
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    return 0;
+  enableRawMode();
+  char c;
+
+  //read 1 byte into var c until there are no more bytes to read or 'q' is pressed
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  return 0;
 }
