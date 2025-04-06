89040af5442cb7cbd90ccca633f59d5f476e0893
Adam Kokhnover <adam.kokhnover@gmail.com>
2025-04-06 17:15:12 +0300

    Added error handling

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <termios.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -7,16 +8,23 @@
 
 struct termios orig_termios; //will hold a copy of the original state of the terminal atts
 
+/*prints an error massage and exits the program*/
+void die(const char *s) {
+  perror(s); //prints the right error according to the global errno variable
+  exit(1); //indicates failure
+}
+
 /* resotres the user's original state of the terminal attributes*/
 void disableRawMode() {
- tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
+    die("tcsetattr");
 }
 
 /*enables raw mode which turns off echoing when the user types
   does this by changing the fourth bit which corresponds to the ECHO bitflag
 */
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios); //read terminal attributes into the termios struct
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr"); //read terminal attributes into the termios struct
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -28,15 +36,15 @@ void enableRawMode() {
   raw.c_cc[VTIME] = 1; // max amount of time to wait before read() returns - 1/10 of a second
 
   //TCSAFLUSH - when to apply the change (discards any unread input before applying the changes to the terminal)
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //apply the modified attributes to the terminal
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("setattr"); //apply the modified attributes to the terminal
 }
 
 int main() {
   enableRawMode();
-  
+
   while(1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
