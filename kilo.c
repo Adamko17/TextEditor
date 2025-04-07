abf4a01702bc5b94a0f5547e1979c9de056f26b7
Adam Kokhnover <adam.kokhnover@gmail.com>
2025-04-07 18:23:42 +0300

    windowsize-v1

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/ioctl.h>
 #include <stdio.h>
 
 /*** defines ***/
@@ -13,7 +14,13 @@
 
 /*** data ***/
 
-struct termios orig_termios; //will hold a copy of the original state of the terminal atts
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios; //will hold a copy of the original state of the terminal atts
+} ;
+
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -29,7 +36,7 @@ void die(const char *s) {
 
 /* resotres the user's original state of the terminal attributes*/
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) 
     die("tcsetattr");
 }
 
@@ -37,10 +44,10 @@ void disableRawMode() {
   does this by changing the fourth bit which corresponds to the ECHO bitflag
 */
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr"); //read terminal attributes into the termios struct
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr"); //read terminal attributes into the termios struct
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //disables ctrl-s and ctrl-q
   raw.c_oflag &= ~(OPOST); // turn off output processing like carriage return or newline 
   raw.c_cflag |= (CS8);
@@ -62,11 +69,26 @@ char editorReadKey() {
   return c;
 }
 
+/* places the number of cols and rows into the pointer vals*/
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    editorReadKey();
+    return -1; //indicating failure
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** output ***/
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
 
   }
@@ -99,6 +121,11 @@ void editorProcessKeypress() {
   }
 }
 /*** init ***/
+
+/* initialize all the fields in the E struct*/
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 
 int main() {
   enableRawMode();
